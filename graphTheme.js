bb5869dad614fdb3db045a0fc0f594ea4880364d
Jacob Asper <jacobasper191@gmail.com>
2022-11-01 00:40:45 -0400

    format code

--- graphTheme.js
+++ graphTheme.js
@@ -1,19 +1,19 @@
-const colorsArr = ["rgb(22, 27, 34)", "rgb(99, 28, 3)", "rgb(189, 86, 29)", "rgb(250, 122, 24)", "rgb(253, 223, 104)"]
+const colorsArr = ['rgb(22, 27, 34)', 'rgb(99, 28, 3)', 'rgb(189, 86, 29)', 'rgb(250, 122, 24)', 'rgb(253, 223, 104)'];
 
-themeContributionGraph(colorsArr)
-themeActivityOverview(colorsArr)
+themeContributionGraph(colorsArr);
+themeActivityOverview(colorsArr);
 
-function themeActivityOverview(colorArr){
-	const style =document.querySelector('.js-highlight-blob').style
-	const color = colorArr[colorArr.length-1]
-	style.fill = color
-	style.stroke = color
+function themeActivityOverview(colorArr) {
+	const { style } = document.querySelector('.js-highlight-blob');
+	const color = colorArr[colorArr.length - 1];
+	style.fill = color;
+	style.stroke = color;
 }
 
-function themeContributionGraph(colorArr){
-	const style = document.querySelector(':root').style;
-	
-	colorArr.forEach((color, i)=>{
-		style.setProperty(`--color-calendar-graph-day-L${i}-bg`, color)
-	})
+function themeContributionGraph(colorArr) {
+	const { style } = document.querySelector(':root');
+
+	colorArr.forEach((color, i) => {
+		style.setProperty(`--color-calendar-graph-day-L${i}-bg`, color);
+	});
 }
