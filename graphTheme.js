0ca198959c88e2bd007e4bed40a07a7d5ef7b56c
Jacob Asper <jacobasper191@gmail.com>
2022-11-01 00:32:29 -0400

    Add themeActivityOverview function and change other to themeContributionGraph

--- graphTheme.js
+++ graphTheme.js
@@ -1,8 +1,16 @@
 const colorsArr = ["rgb(22, 27, 34)", "rgb(99, 28, 3)", "rgb(189, 86, 29)", "rgb(250, 122, 24)", "rgb(253, 223, 104)"]
-changeColors(colorsArr)
 
+themeContributionGraph(colorsArr)
+themeActivityOverview(colorsArr)
 
-function changeColors(colorArr){
+function themeActivityOverview(colorArr){
+	const style =document.querySelector('.js-highlight-blob').style
+	const color = colorArr[colorArr.length-1]
+	style.fill = color
+	style.stroke = color
+}
+
+function themeContributionGraph(colorArr){
 	const style = document.querySelector(':root').style;
 	
 	colorArr.forEach((color, i)=>{
